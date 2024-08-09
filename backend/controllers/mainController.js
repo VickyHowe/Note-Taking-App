8c8600393d3130e465606ab8bae68764a79f5549
Vicky Howe <mcvhowe@gmail.com>
2024-08-08 19:48:37 -0700

    updatd notes

---
+++ backend/controllers/mainController.js
@@ -0,0 +1,28 @@
+/**
+ * Get /
+ * Homepage
+ */
+
+exports.homepage = async (req, res) => {
+    const locals = {
+        title: 'NodeJs Notes',
+        description: 'Free NodeJs Notes App'
+    }
+    res.render('index', {
+        locals,
+        layout: '../views/layouts/front-page'
+    });
+};
+
+/**
+ * Get /
+ * About
+ */
+
+exports.about = async (req, res) => {
+    const locals = {
+        title: 'About - NodeJs Notes',
+        description: 'Free NodeJs Notes App'
+    }
+    res.render('about',  locals);
+};
